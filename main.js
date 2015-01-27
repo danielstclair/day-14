81e80b5771fdb7dc49e222796fc3185158e5e770
Daniel St. Clair <danielallanstclair@gmail.com>
2015-01-26 18:59:31 -0600

    adding console.log features for api

--- main.js
+++ main.js
@@ -7,8 +7,53 @@ function onReady() {
 	function onSearchButtonClick() {
 		// 1. Input value
 		console.log($('#search-box').val());
-		$('#results').html($('#search-box').val());
-		$('#results').html($('#search-box').val())
+		$('#results').html($('#search-box').val() + "<br>");
+		// $('#results').html($('#search-box').val())
+
+		function imdbSearch(a) {
+		$.get(
+			'http://www.omdbapi.com',
+			{
+				s: $('#search-box').val()
+				// y: year,
+				// type: type,
+				// tomatoes: tomatoes
+			},
+			onSearchResults,
+			'json'
+		);
 	}
 
-	
+		function tomatoMeter(x) {
+			$.get(
+				'http://www.omdbapi.com',
+				{
+					i: x,
+					tomatoes: true
+				},
+				onTomatoResults,
+				'json'
+			);
+		}
+
+		function onSearchResults(data) {
+			console.log(data);
+			console.log(data.Search[0].imdbID);
+			tomatoMeter(data.Search[0].imdbID);
+			// for (var i = 0; i < data.Search.length; i++) {
+			// 	$('#results').append("<br>" + data.Search[i].Title + "<br>" + data.Search[i].Year + "<br>");
+			// };
+			// $('#results').html(data.Search[0].Title + "<br>" + data.Search[0].Year);
+		}
+
+		function onTomatoResults(data) {
+			console.log(data);
+			console.log(data.Genre);
+			// $('#results').html(data.Title + "<br>" + data.Year + "<br>" + data.Actors);
+		}
+
+		console.log(imdbSearch());
+		// $('#results').html(data.Title.Year.Actors);
+		}
+}
+
